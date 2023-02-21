fd93f4e10e0228e3eeebf89baba7680ccafa9879
keagud <keagud@protonmail.com>
2023-02-21 14:05:49 -0800

    move search to be oopier

--- linelog/log_util.py
+++ linelog/log_util.py
@@ -1,12 +1,11 @@
 import re
 from itertools import dropwhile
 import datetime
-from datetime import datetime as dt
+from datetime import date, datetime as dt
 import json
 from os.path import splitext
-from os import PathLike
+from os import PathLike, getcwd
 from typing import NamedTuple
-
 
 from functools import partial
 from functools import reduce
@@ -18,7 +17,7 @@ import pygit2
 from sloc import sloc_from_text
 
 
-def is_commit_on_date(commit, target_date: datetime.date) -> bool:
+def is_commit_on_date(commit: pygit2.Commit, target_date: datetime.date) -> bool:
 
     interval_start = dt.combine(target_date, datetime.time.min)
 
@@ -147,7 +146,7 @@ def sum_dicts(d1: dict, d2: dict):
     return {k: combined[k] for k in combined if combined[k]}
 
 
-#TODO maybe it makes more sense for this to be wrapped up in a class?
+# TODO maybe it makes more sense for this to be wrapped up in a class?
 def get_interval_repo_lines_custom(
     repo: pygit2.Repository,
     start_date: datetime.date,
@@ -207,21 +206,163 @@ def _get_ignore_config():
     return ignore_config
 
 
-
 get_interval_repo_lines = partial(
     get_interval_repo_lines_custom,
     ignore_patterns=_get_ignore_config(),
     filetypes_db=_load_file_config(),
 )
 
-def recursive_repo_search(path: PathLike):
+
+def get_global_username() -> str:
+    config = pygit2.Config.get_global_config()
+    return config._get_entry("user.name").value
 
 
+class RepoScanner:
+    default_filetype_db = "filetypes.json"
+
+    def __init__(
+        self,
+        username: str | None = None,
+        filetypes_db_path: str | None = None,
+        ignore_patterns: list[str] | None = None,
+    ):
+
+        if ignore_patterns is None:
+            self.ignore_patterns = []
+
+        else:
+            self.ignore_patterns = ignore_patterns
+
+        self.username = username
+        if self.username is None:
+            self.username = get_global_username()
+
+        self.filetypes_db_path = filetypes_db_path
+
+        if self.filetypes_db_path is None:
+            self.filetypes_db_path = self.default_filetype_db
+
+        with open(self.filetypes_db_path, "r") as dbfile:
+            self.filetypes_db = json.load(dbfile)
+
+    def load_file_config(self):
+        pass
+
+    def get_repo_lines(self, repo: pygit2.Repository):
+        pass
+
+    def files_line_sum(
+        self,
+        commit: pygit2.Commit | None,
+    ):
+
+        if commit is None:
+            return {}
+
+        commit_files = self.get_tree_files(commit.tree)
+
+        data_by_type: dict[str, int] = {}
+
+        for file in commit_files:
+            if not file.name:
+                continue
+
+            filename = file.name
+            _, ext = splitext(filename)
+            if not ext:
+                continue
+
+            cleaned_ext = ext.strip(".").lower()
+
+            matched_filetype = self.filetypes_db.get(cleaned_ext, "Unknown")
+
+            file_lines = sloc_from_text(filename, file.data)
+
+            if not matched_filetype in data_by_type:
+                data_by_type[matched_filetype] = file_lines
+                continue
+
+            data_by_type[matched_filetype] += file_lines
+
+        return data_by_type
+
+    def get_tree_files(
+        self,
+        tree_root: pygit2.Tree,
+    ):
+        contained_files: list[pygit2.Blob] = []
+        for item in tree_root:
+
+            if item.name is None:
+                continue
+
+            matched_ignores = map(
+                lambda p: re.match(p, str(item.name)), self.ignore_patterns
+            )
+
+            if any(matched_ignores):
+                continue
+
+            if isinstance(item, pygit2.Blob) and not item.is_binary:
+                contained_files.append(item)
+
+            elif isinstance(item, pygit2.Tree):
+                contained_files.extend(self.get_tree_files(item))
+
+        return contained_files
+
+    def get_repo_stats(
+        self,
+        repo: pygit2.Repository | str,
+        start_date: datetime.date,
+        end_date: datetime.date | None = None,
+    ) -> dict[datetime.date, dict[str, int]]:
 
 
+        if isinstance(repo, str):
+            repo = pygit2.Repository(repo)
+
+
+        if end_date is None:
+            end_date = start_date + datetime.timedelta(days=1)
+
+        def iter_days(start: datetime.date, end: datetime.date):
+            days = end.toordinal() - start.toordinal()
+
+            iter_delta = datetime.timedelta(days=-1 if days < 0 else 1)
+
+            iter_day = start
+
+            while iter_day != end:
+                yield iter_day
+                iter_day += iter_delta
+
+        totals: dict[datetime.date, dict[str, int]] = {}
+        for d in iter_days(start_date, end_date):
+
+            day_results = [files_line_sum(c) for c in get_date_commits(repo, d)]
+
+            commit_line_changes = [
+                compare_commit_totals(e, l) for e, l in pairwise(reversed(day_results))
+            ]
+
+            totals[d] = reduce(sum_dicts, commit_line_changes, {})
+
+        return totals
+
+
+def run():
+
+    ignore_config = [r".*\.txt", r".*\.md", r".*\.rst", r".*\.toml", r".*\.json"]
+
+    w = RepoScanner(ignore_patterns=ignore_config)
+    current = getcwd()
+
+    s= w.get_repo_stats(current, date(2023, 2,19))
+    print(s)
     
 
 
-
-
-
+if __name__ == "__main__":
+    run()
